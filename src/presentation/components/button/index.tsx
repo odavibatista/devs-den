aa1605ebe850082c11243db484e04235dc751225
Davi Deosmar Batista Oliveira Miranda - 823212382 <823212382@ulife.com.br>
2024-04-16 21:19:02 -0300

    feat: created skeleton of button component

---
+++ src/presentation/components/button/index.tsx
@@ -0,0 +1,12 @@
+import styles from './styles.module.scss'
+
+
+const Button = (): JSX.Element => {
+
+    return(
+        <div className={styles.button}>
+        </div>
+    )
+}
+
+export default Button
