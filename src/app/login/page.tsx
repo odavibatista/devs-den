a4f14aee4daaf54f2f78f3b97fa583c66a84d351
Davi Batista <davideosmar11@gmail.com>
2024-04-22 19:48:26 -0300

    feat: cleared code for login view

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -3,18 +3,16 @@ import styles from './styles.module.scss'
 
 export default function LoginScreen() {
   return (
-    <div className={styles.mainLogin}>
+    <section className={styles.mainLogin}>
         <div className={styles.leftLogin}>
-            <img src="rapazzz.svg" alt="jeff-bezos" />
-            <h1><br/>JEFFREY BEZOS</h1>
-            <text>
+            <img src="/login/jeff-bezos.jpg" alt="jeff-bezos" className={styles.avatar} />
+            <h1 className={styles.ceoName}>JEFFREY BEZOS</h1>
+            <text className={styles.ceoTestimony}>
               “Graças ao Dev’s Den, minha equipe conseguiu achar os desenvolvedores certos em tempo recorde! As ferramentas do site ajudam que o candidato certo venha para a vaga certa. Não tem erro!”
             </text>
         </div>
         <div className={styles.rightLogin}>
-            <div className={styles.cardLogin}>
                 <div className={styles.textField}>
-                    <h1>LOGIN</h1>
                     <label htmlFor="usuario">Usuario</label>
                     <input type="text" name="Usuario" placeholder="Usuario" />
                 </div>
@@ -22,9 +20,8 @@ export default function LoginScreen() {
                     <label htmlFor="senha">Senha</label>
                     <input type="password" name="senha" placeholder="Senha" />
                 </div>
-                <Button text="Login"  />
-            </div>
+                <Button text="ENTRAR"  />
         </div>
-    </div>
+    </section>
   );
 }
