29ee09bec1f3d3ffbc0c6219f9e02b6f426ca679
Davi Batista <davideosmar11@gmail.com>
2024-04-22 21:05:21 -0300

    feat: created category badge component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Button from "@/presentation/components/button";
+import CategoryBadge from "@/presentation/components/category-badge";
 
 export default function Home() {
   return (
     <main>
       <Button text="Login" />
+      <CategoryBadge imageUrl="/category-badges/frontend.svg" name="Teste" />
     </main>
   );
 }
--- src/presentation/components/category-badge/index.tsx
+++ src/presentation/components/category-badge/index.tsx
@@ -10,7 +10,7 @@ const CategoryBadge = ({ name, imageUrl }: CategoryBadgeProps): JSX.Element => {
 
     return(
         <div className={styles.categoryBadge}>
-            <Image src={imageUrl} alt={name}/>
+            <Image src={imageUrl} alt={name} width={135} height={135} />
             <h4 className={styles.h4}>
                 {name}
             </h4>
