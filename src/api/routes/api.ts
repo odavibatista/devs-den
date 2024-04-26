fbf4622a3e9d0791e45dbfeaf20b0d95ea91c0f5
Davi Batista <davideosmar11@gmail.com>
2024-04-25 21:33:44 -0300

    docs: novos comentários no código, remoção da dotenv

--- src/api/routes/api.ts
+++ src/api/routes/api.ts
@@ -1,9 +1,12 @@
 import axios from "axios";
 
+/* Aqui, chamamos a url que inserimos no arquivo '.env' para ser utilizado pelo Axios em todas as nossas requisições */
 const apiURL: string = String(process.env.API_URL?.toString())
 
+/* Aqui, criamos a API passando como URL o dado obtido acima, para que todas as requisições sejam nesse endereço */
 const api = axios.create({
     baseURL: apiURL
 })
 
+/* Exportamos para ela ser a base de todas as nossas requisições */
 export default api
--- src/api/routes/user/userLogin.ts
+++ src/api/routes/user/userLogin.ts
@@ -1,17 +1,24 @@
+/* Aqui, importamos a API criada anteriormente */
 import api from "../api";
 
+/* Passamos os atributos do login, por hora e provavelmente somente e-mail e senha, para que seja tipado para a função*/
 interface userLoginAttributes   {
     email: string
     password: string
 }
 
+/* Aqui, declaramos a função userLogin() como o service que irá chamar no endpoint, passando um parâmetro do tipo criado acima */
 const userLogin = async (data: userLoginAttributes) => {
     
+    /* Chamamos o endpoint '/user/login' enviando os dados do tipo declarado */
     const response = await api.post("/user/login", data).catch((err)    =>  {
+        /* Caso haja um erro, retornamos ele */
         return err.response
     })
 
+    /* Retornamos a resposta contendo os dados se tudo der certo */
     return response.data
 }
 
+/* Exportamos a função para ser utilizada em outros arquivos */
 export default userLogin
