24120d4261753cbdd60f02a27eccd75f2da4df74
KaatoDev <kaatodev@gmail.com>
2024-04-30 17:58:35 -0300

    y0.1
    User-terms and privacy-policy embeds.

---
+++ src/presentation/components/privacy-policy/index.tsx
@@ -0,0 +1,31 @@
+import styles from './styles.module.scss'
+
+export default function PrivacePolicy() {
+    return(
+        <div className={styles.back}>
+            <div className={styles.terms}>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus accusantium culpa doloremque
+                    dolorum eius eligendi enim facilis in ipsam iure iusto laborum magni maiores modi nemo, neque
+                    nesciunt quia similique.
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus accusantium culpa doloremque
+                    dolorum eius eligendi enim facilis in ipsam iure iusto laborum magni maiores modi nemo, neque
+                    nesciunt quia similique.
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus accusantium culpa doloremque
+                    dolorum eius eligendi enim facilis in ipsam iure iusto laborum magni maiores modi nemo, neque
+                </p>
+                <br/>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus accusantium culpa doloremque
+                    dolorum eius eligendi enim facilis in ipsam iure iusto laborum magni maiores modi nemo, neque
+                    nesciunt quia similique.
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus accusantium culpa doloremque
+                    dolorum eius eligendi enim facilis in ipsam iure iusto laborum magni maiores modi nemo, neque
+                    nesciunt quia similique.
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus accusantium culpa doloremque
+                    dolorum eius eligendi enim facilis in ipsam iure iusto laborum magni maiores modi nemo, neque
+                </p>
+            </div>
+        </div>
+    )
+}
