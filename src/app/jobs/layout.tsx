6c96fd17e34ba8ac584c4113269b8daddcb41704
Davi Deosmar Batista Oliveira Miranda - 823212382 <823212382@ulife.com.br>
2024-05-15 20:29:47 -0300

    feat: created structure for candidate register component

--- src/app/jobs/layout.tsx
+++ src/app/jobs/layout.tsx
@@ -4,7 +4,7 @@ import JobsScreen from "./page";
 
 export const metadata: Metadata = {
   title: "Dev's Den - Vagas",
-  description: "Navegue por vagas de empresas que buscam seus talentos.."
+  description: "Encontre a vaga que mais combina com as suas habilidades."
 };
 
 export default function RootLayout({
