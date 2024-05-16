2d05d7410cf3c2a8f011d914ad752f4018f6094b
Davi Batista <davideosmar11@gmail.com>
2024-05-15 23:09:46 -0300

    feat: created authenticated routes layout

---
+++ src/app/(authenticated-routes)/layout.tsx
@@ -0,0 +1,30 @@
+'use client'
+
+import { useHome } from "@/providers/home-data-provider";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+    const { homeData, isHomeDataLoading } = useHome();
+
+    const router = useRouter()
+    
+    useEffect(() => {
+        (async () => {
+            if (!homeData && !isHomeDataLoading) {
+                router.push("/")
+            }
+        })()
+      })
+
+
+  return (
+    <>
+        {children}
+    </>
+  );
+}
