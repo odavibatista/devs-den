b11cd65e1a8be9a76b941703de84b7af856f0198
Davi Deosmar Batista Oliveira Miranda - 823212382 <823212382@ulife.com.br>
2024-05-15 21:39:33 -0300

    fix: fixed type on get skills promise

--- src/api/routes/skills/getSkills.ts
+++ src/api/routes/skills/getSkills.ts
@@ -1,8 +1,7 @@
-import { ApiError } from "next/dist/server/api-utils";
-import api from "../api";
+import api, { IAPIError } from "../api";
 import { IGetSkill } from "./getSingleSkill";
 
-const getSkills = async(): Promise<IGetSkill | ApiError> => {
+const getSkills = async(): Promise<IGetSkill | IAPIError> => {
     
     const response = await api.get('/skills').catch((err)    => {
         return err.response
