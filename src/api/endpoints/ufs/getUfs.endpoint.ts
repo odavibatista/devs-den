c7e4d0990628b86ab2535fee93c0e4d644fae749
Davi Batista <davideosmar11@gmail.com>
2024-05-17 09:27:12 -0300

    feat: created getUfs endpoint

---
+++ src/api/endpoints/ufs/getUfs.endpoint.ts
@@ -0,0 +1,17 @@
+import api, { IAPIError } from "../api";
+export interface IGetUF {
+    id_uf: number
+    name: string
+    acronym: string
+}
+
+const getUfs = async(): Promise<IGetUF[] | IAPIError> => {
+    
+    const response = await api.get('/ufs').catch((err)    => {
+        return err.response
+    })
+
+    return response.data
+} 
+
+export default getUfs
